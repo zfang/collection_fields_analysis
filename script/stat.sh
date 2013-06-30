47e76e66eff503818280e53d44de13e2c3f95ea9
Felix Fang <felixfangzh@gmail.com>
2013-06-30 14:25:14 -0400

    fixed stat.sh

--- script/stat.sh
+++ script/stat.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 nonAliasedFields=0
-for i in `cat $@ | sed -rn 's/.* nonAliasedFields size: ([0-9]+)/\1/p'`; do
+for i in `cat $@ | sed -rn 's/.*nonAliasedFields size: ([0-9]+)/\1/p'`; do
    nonAliasedFields=$(( $nonAliasedFields + $i ))
 done
 echo "Total number of nonAliasedFields fields: $nonAliasedFields"
 
-aliasedFieldStore=0
-for i in `cat $@ | sed -rn 's/.* aliasedFieldStore size: ([0-9]+)/\1/p'`; do
-   aliasedFieldStore=$(( $aliasedFieldStore + $i ))
+aliasedFields=0
+for i in `cat $@ | sed -rn 's/.*aliasedFields size: ([0-9]+)/\1/p'`; do
+   aliasedFields=$(( $aliasedFields + $i ))
 done
-echo "Total number of aliasedFieldStore fields: $aliasedFieldStore"
+echo "Total number of aliasedFields fields: $aliasedFields"
 
 unknownFields=0
-for i in `cat $@ | sed -rn 's/.* unknownFields size: ([0-9]+)/\1/p'`; do
+for i in `cat $@ | sed -rn 's/.*unknownFields size: ([0-9]+)/\1/p'`; do
    unknownFields=$(( $unknownFields + $i ))
 done
 echo "Total number of unknownFields fields: $unknownFields"
